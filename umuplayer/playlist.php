a388fe6b1603cb767ac3b646ef3bcc391311b442
diller444 <68323133+diller444@users.noreply.github.com>
2022-01-03 00:38:05 +0300

    Update playlist.php

--- umuplayer/playlist.php
+++ umuplayer/playlist.php
@@ -12,7 +12,7 @@ $content = curl_exec($ch);
 curl_close($ch);
 preg_match_all('/data: \'([\s\S]+?)\'}/', $content, $p);
 $json = json_decode(stripcslashes($p[1][1]));
-echo '<center><img src="'.$json->header->musicDetailHeaderRenderer->thumbnail->croppedSquareThumbnailRenderer->thumbnail->thumbnails[1]->url.'">';
+echo '<center><img src="'.preg_replace('/https:\/\/(.*?)\/(.*?)$/','https://pipedproxy-bom.kavin.rocks/$2?host=$1',$json->header->musicDetailHeaderRenderer->thumbnail->croppedSquareThumbnailRenderer->thumbnail->thumbnails[1]->url).'">';
 echo '<h2>'.$json->header->musicDetailHeaderRenderer->title->runs[0]->text.'</h2>';
 foreach($json->header->musicDetailHeaderRenderer->subtitle->runs as $text){
     echo $text->text;
@@ -24,8 +24,8 @@ if(!$page){$album=true;$page = $json->contents->singleColumnBrowseResultsRendere
 foreach ($page as $sub){
 echo '<div><a style="display:block;" href="/watch?v='.$sub->musicResponsiveListItemRenderer->flexColumns[0]->musicResponsiveListItemFlexColumnRenderer->text->runs[0]->navigationEndpoint->watchEndpoint->videoId.'&list='.$sub->musicResponsiveListItemRenderer->flexColumns[0]->musicResponsiveListItemFlexColumnRenderer->text->runs[0]->navigationEndpoint->watchEndpoint->playlistId.'">';
 if($album){
-    echo '<div style=display:inline-block;padding:8px;vertical-align:middle;><img width=60 src="'.$json->header->musicDetailHeaderRenderer->thumbnail->croppedSquareThumbnailRenderer->thumbnail->thumbnails[0]->url.'"></div>';
-}else{echo '<div style=display:inline-block;padding:8px;vertical-align:middle;><img width=60 src="'.$sub->musicResponsiveListItemRenderer->thumbnail->musicThumbnailRenderer->thumbnail->thumbnails[0]->url.'"></div>';}
+    echo '<div style=display:inline-block;padding:8px;vertical-align:middle;><img width=60 src="'.preg_replace('/https:\/\/(.*?)\/(.*?)$/','https://pipedproxy-bom.kavin.rocks/$2?host=$1',$json->header->musicDetailHeaderRenderer->thumbnail->croppedSquareThumbnailRenderer->thumbnail->thumbnails[0]->url).'"></div>';
+}else{echo '<div style=display:inline-block;padding:8px;vertical-align:middle;><img width=60 src="'.preg_replace('/https:\/\/(.*?)\/(.*?)$/','https://pipedproxy-bom.kavin.rocks/$2?host=$1',$sub->musicResponsiveListItemRenderer->thumbnail->musicThumbnailRenderer->thumbnail->thumbnails[0]->url).'"></div>';}
 echo '<div style="display:inline-block;padding:8px;vertical-align:middle;width:calc(100% - 120px);overflow-x:scroll;"><b>'.$sub->musicResponsiveListItemRenderer->flexColumns[0]->musicResponsiveListItemFlexColumnRenderer->text->runs[0]->text.'</b><br>';
 if($album){
 echo $sub->musicResponsiveListItemRenderer->flexColumns[2]->musicResponsiveListItemFlexColumnRenderer->text->runs[0]->text;
